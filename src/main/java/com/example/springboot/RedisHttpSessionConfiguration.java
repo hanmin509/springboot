d90ed6fedbe4f9d50c654931e6d1f65c735fa6e9
hanmin509 <hanmin509@naver.com>
2021-03-28 15:48:12 +0900

    redis pub/sub test

--- src/main/java/com/example/springboot/RedisHttpSessionConfiguration.java
+++ src/main/java/com/example/springboot/RedisHttpSessionConfiguration.java
@@ -12,11 +12,10 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
 @Configuration
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60)
+//@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 6000)
 public class RedisHttpSessionConfiguration extends AbstractHttpSessionApplicationInitializer {
 
     public RedisHttpSessionConfiguration() {
---
+++ src/main/java/com/example/springboot/message/service/RedisPublisher.java
@@ -0,0 +1,18 @@
+package com.example.springboot.message.service;
+
+import com.example.springboot.message.vo.RoomMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisPublisher {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    public void publish(ChannelTopic topic, RoomMessage message) {
+        redisTemplate.convertAndSend(topic.getTopic(), message);
+    }
+}
